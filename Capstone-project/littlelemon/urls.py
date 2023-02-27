d8e1c064882134396e3ec53b8aa3baa7bfcc1aa1
vmunozrivera <35176705+vmunozrivera@users.noreply.github.com>
2023-02-27 11:44:50 -0400

    Static content and models

---
+++ Capstone-project/littlelemon/urls.py
@@ -0,0 +1,10 @@
+"""littlelemon URL Configuration """
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('restaurant/', include('restaurant.urls')),
+]
---
+++ Capstone-project/restaurant/models.py
@@ -0,0 +1,20 @@
+
+from django.db import models
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.title
